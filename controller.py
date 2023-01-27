6183e890c9cc1f5fbab444336e9684ecd079eb65
Mayya <msalosina@mail.ru>
2023-01-27 22:16:13 +0700

    add controller

---
+++ controller.py
@@ -0,0 +1,19 @@
+import view
+import model
+
+def start():
+    model.set_class(view.input_class())
+    model.set_subject(view.input_subject())
+    model.open_file()
+
+    student = ''
+    while True:
+        journal = model.get_gournal()
+        view.list_of_child(journal)     # показываем журнал
+        student = view.who_answer()     # кто отвечает
+        if student == 'exit':
+            break
+        mark = int(view.what_mark())    # на какую оценку ответил
+        model.student_mark(student, mark)   # занесли в журнал
+
+
