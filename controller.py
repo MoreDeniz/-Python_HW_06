9e227b7dd55abd5a85d940dc9204a8e015c03a59
Mayya <msalosina@mail.ru>
2023-01-30 10:57:21 +0700

    controller changed

--- controller.py
+++ controller.py
@@ -3,9 +3,9 @@ import model
 
 def start():
     model.set_class(view.input_class())
+    # model.set_class(model.choose())
     model.set_subject(view.input_subject())
     model.open_file()
-
     student = ''
     while True:
         journal = model.get_gournal()
@@ -15,5 +15,5 @@ def start():
             break
         mark = int(view.what_mark())    # на какую оценку ответил
         model.student_mark(student, mark)   # занесли в журнал
-
+    model.save_file()
 
